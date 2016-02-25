789829eb6bd37f63b142b885ebfd85d437b1d604
naverstay <3a4f044785G->
2016-02-25 11:08:37 +0200

    company

--- js/company.js
+++ js/company.js
@@ -1,7 +1,9 @@
 var popupOrderItem,
     controlPanelBtn,
     popupBtn,
+    active_tab = 0,
     heroSlider,
+    about_tabs,
     pageSliderDensity = 30,
     pageSlider,
     pageSliderLoaded = false,
@@ -63,7 +65,6 @@ var popupOrderItem,
 
             body_var.toggleClass('show_go_top header_fixed', nextIndex != 1);
 
-
         },
         afterLoad: function (anchorLink, index) {
             console.log(anchorLink, index);
@@ -77,6 +78,8 @@ var popupOrderItem,
         },
         afterRender: function () {
             pageSliderLoaded = true;
+
+            init_tabs();
         },
         afterResize: function () {
         },
@@ -100,21 +103,53 @@ $(function ($) {
         }
 
         return false;
+    }).delegate('.asideSelect', 'change', function () {
+        $('a[href=' + $(this).val() + ']').click();
     });
 
-
     init_select();
+
+    init_tabs();
 
 });
 
 function init_select() {
+    var asideSelect = $('.asideSelect');
+
     $('.select2.select2-container').remove();
 
-    $('.asideSelect').select2({
+    asideSelect.select2({
         minimumResultsForSearch: Infinity,
         dropdownParent: $('#aside_menu_dropdown'),
         width: '100%'
     });
+
+    asideSelect.find('option').eq(active_tab).attr('selected', 'selected');
+
+    asideSelect.trigger('change');
+
+}
+
+function init_tabs() {
+    console.log(about_tabs);
+    if (about_tabs != void 0) {
+        about_tabs.tabs("destroy");
+    }
+
+    setTimeout(function () {
+        var tabBlock = $('.tabBlock'),
+            about_tabs = tabBlock.tabs({
+                active: active_tab,
+                tabContext: tabBlock.data('tab-context'),
+                activate: function (e, ui) {
+                    active_tab = ui.newPanel.index();
+                    console.log(active_tab, ui.newPanel.attr('id'));
+                    //$('.asideSelect option[value=#' + ui.newPanel.attr('id') + ']').attr('selected', 'selected');
+                    $('.asideSelect').val('#' + ui.newPanel.attr('id')).trigger('change');
+                }
+            });
+
+    }, 5);
 
 }
 
@@ -152,8 +187,9 @@ $(window).on('load', function () {
                 cell.after(cell.html());
                 cell.remove();
             });
-            
+
             init_select();
+            init_tabs();
 
         }
     } else {
