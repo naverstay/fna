048f45017ecddf8b79b6c367978435698914f332
naverstay <3a4f044785G->
2016-03-22 11:53:29 +0200

    price tabs

--- js/prices.js
+++ js/prices.js
@@ -2,28 +2,20 @@ var price_tabs;
 
 $(function ($) {
 
+    $('.select2').select2({
+        minimumResultsForSearch: Infinity,
+        width: '100%'
+    });
 
     $('.priceSelect').select2({
         minimumResultsForSearch: Infinity,
-        dropdownParent: $('#aside_menu_dropdown'),
+        dropdownParent: $('#price_menu_dropdown'),
         width: '100%'
     });
 
     body_var.delegate('.priceSelect', 'change', function () {
         $('a[href=' + $(this).val() + ']').click();
     });
-
-    $('.contactOfficeBtn').on('click', function () {
-        var firedEl = $(this);
-
-        firedEl.parent().addClass('active').siblings().removeClass('active');
-
-        $('.officeTime').hide().filter(function (i, el) {
-            return $(el).hasClass(firedEl.attr('data-href'));
-        }).show();
-
-        return false;
-    }).first().click();
 
     var tabBlock = $('.tabBlock'),
         price_tabs = tabBlock.tabs({
@@ -34,15 +26,10 @@ $(function ($) {
 
                 tab.parent().attr('data-active-tab', tab.index());
 
-                $('.priceSelect').val('#' + ui.newPanel.attr('id')).trigger('change');
+                //$('.priceSelect').val('#' + ui.newPanel.attr('id')).trigger('change');
 
             }
         });
-
-    $('.select2').select2({
-        minimumResultsForSearch: Infinity,
-        width: '100%'
-    });
 
     $('#annual_revenu').slider({
         min: 1000,
@@ -51,9 +38,6 @@ $(function ($) {
         slide: function (event, ui) {
             $(ui.handle).find('.toddler_val').text(ui.value + ' тыс. руб.');
             $('#annual_revenu_val').val(ui.value);
-        },
-        load: function (event, ui) {
-            console.log(event, ui);
         }
     });
 
